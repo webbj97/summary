146cf2f0250dc8fb61cd8d57e84f13464e907f3b
yuguang <yuguang@gaoding.com>
2020-06-02 16:15:38 +0800

    :art:打算优化文章+目录

--- _book/JavaScript设计模式/index.js
+++ _book/JavaScript设计模式/index.js
@@ -1,9 +1,26 @@
-/*
- * @desc:
- * @Author: 余光
- * @Email: webbj97@163.com
- * @Date: 2020-05-21 13:53:01
- */
-var animal = function (type) {
-
+var foo = {
+    value: 1
 }
+function show(a, b) {
+    console.log(this.value);
+    console.log(a + b)
+}
+
+// Function.prototype.setCall = function (obj) {
+//     console.log(this); // 此时this指向show
+//     obj.func = this; // 将函数变成对象的内部属性
+//     obj.func(obj.value) // 指定函数
+//     delete obj.func // 删除函数，当做什么都没发生～
+// }
+
+Function.prototype.setCall = function (obj) {
+    obj.fn = this; // 将函数变成对象的内部属性
+    var args = [];
+    for(let i = 1; i < arguments.length; i++){
+        args.push('arguments[' + i + ']');
+    }
+    eval('obj.fn(' + args + ')'); // 传入参数
+    delete obj.fn; // 删除函数，当做什么都没发生～
+}
+
+show.setCall(foo, 1, 2)
