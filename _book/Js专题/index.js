73541f9440e62f724000fc7b10dfeac3db54a74a
yuguang <yuguang@gaoding.com>
2020-08-12 19:03:17 +0800

    ✨添加深浅拷贝

--- _book/Js专题/index.js
+++ _book/Js专题/index.js
@@ -1,4 +1,13 @@
-var arr = [NaN, NaN, NaN, 1, 2]
-arr.forEach(item => {
-    console.log('item:', item);
-});
+/*
+ * @desc:
+ * @Author: 余光
+ * @Email: webbj97@163.com
+ * @Date: 2019-11-22 10:20:48
+ */
+var data = [1, 2, 3, 4, 5];
+
+var newData = data;
+newData[0] = 100;
+
+console.log(data[0])
+console.log(newData[0])
