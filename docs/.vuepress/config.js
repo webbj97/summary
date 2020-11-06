e06a00ceb6d2560d64c006ac8998c809d9716a1d
yuguang <yuguang@gaoding.com>
2020-11-06 18:44:39 +0800

    :art:迁移至vuepress - 半成品

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,61 @@
+module.exports = {
+    title: '余光的前端进阶笔记',
+    description: '在这里一定有能帮助你成长的文章',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: '基础|进阶', link: '/blog/' },
+            { text: 'GitHub', link: 'https://github.com/webbj97/summary' },
+        ],
+        markdown: {
+            lineNumbers: true // 代码块显示行号
+        },
+        sidebar: [{
+                title: 'JavaScript基础', // 必要的
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 2, // 可选的, 默认值是 1
+                children: [
+                    '/blog/js-basics/1',
+                    '/blog/js-basics/2',
+                    '/blog/js-basics/3',
+                    '/blog/js-basics/4',
+                    '/blog/js-basics/5',
+                    '/blog/js-basics/6',
+                    '/blog/js-basics/7',
+                    '/blog/js-basics/8',
+                    '/blog/js-basics/9',
+                    '/blog/js-basics/10',
+                ]
+            },
+            {
+                title: 'JavaScript专题', // 必要的
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 2, // 可选的, 默认值是 1
+                children: [
+                    '/blog/js-special/1',
+                ]
+            },
+            {
+                title: 'JavaScript设计模式', // 必要的
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 2, // 可选的, 默认值是 1
+                children: [
+                    '/blog/js-sjms/1',
+                    '/blog/js-sjms/2',
+                    '/blog/js-sjms/3',
+                    '/blog/js-sjms/4',
+                    '/blog/js-sjms/5',
+                    '/blog/js-sjms/6',
+                ]
+            },
+            {
+                title: 'ES6', // 必要的
+                collapsable: true, // 可选的, 默认值是 true,
+                sidebarDepth: 2, // 可选的, 默认值是 1
+                children: [
+                    '/blog/es6/1',
+                ]
+            },
+        ]
+    }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,17 +1,14 @@
-# Hello VuePress
-
-
 ---
 home: true
-heroImage: /logo.jpg
-actionText: 快速上手 →
-actionLink: /zh/guide/
+heroImage: title.jpg
+actionText: 快速开始
+actionLink: /
 features:
-- title: 简洁至上
-  details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
+- title: 前端基础
+  details: 这里有着前端基础——JavaScript的最全面的基础总结，帮助你快速打牢基础，进阶高级前端指日可待！
 - title: Vue驱动
   details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-- title: 高性能
+- title: 框架通识
   details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: MIT Licensed | Copyright © 2018-present Evan You
+footer: MIT Licensed | Copyright © 2020-present 余光
 ---
