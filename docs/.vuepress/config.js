4af8ad17cd8be0429fa111831c87684a522afac4
yuguang <yuguang@gaoding.com>
2020-11-20 11:02:10 +0800

    :art:提交之前的改动

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,68 +9,90 @@ module.exports = {
         nav: [
             { text: 'Home', link: '/' },
             { text: '基础|进阶', link: '/blog/' },
+            { text: '算法|进阶', link: '/algorithm/' },
             { text: 'GitHub', link: 'https://github.com/webbj97/summary' },
         ],
         markdown: {
             lineNumbers: true // 代码块显示行号
         },
-        sidebar: [{
-                title: 'JavaScript基础', // 必要的
-                collapsable: true, // 可选的, 默认值是 true,
-                sidebarDepth: 2, // 可选的, 默认值是 1
-                children: [
-                    '/blog/js-basics/1',
-                    '/blog/js-basics/2',
-                    '/blog/js-basics/3',
-                    '/blog/js-basics/4',
-                    '/blog/js-basics/5',
-                    '/blog/js-basics/6',
-                    '/blog/js-basics/7',
-                    '/blog/js-basics/8',
-                    '/blog/js-basics/9',
-                    '/blog/js-basics/10',
-                ]
-            },
-            {
-                title: 'JavaScript专题', // 必要的
-                collapsable: true, // 可选的, 默认值是 true,
-                sidebarDepth: 2, // 可选的, 默认值是 1
-                children: [
-                    '/blog/js-special/1',
-                    '/blog/js-special/2',
-                    '/blog/js-special/3',
-                    '/blog/js-special/4',
-                    '/blog/js-special/5',
-                    '/blog/js-special/6',
-                    '/blog/js-special/7',
-                    '/blog/js-special/8',
-                    '/blog/js-special/9',
-                ]
-            },
-            {
-                title: 'JavaScript设计模式（待优化）', // 必要的
-                collapsable: true, // 可选的, 默认值是 true,
-                sidebarDepth: 2, // 可选的, 默认值是 1
-                children: [
-                    '/blog/js-sjms/1',
-                    '/blog/js-sjms/2',
-                    '/blog/js-sjms/3',
-                    '/blog/js-sjms/4',
-                    '/blog/js-sjms/5',
-                    '/blog/js-sjms/6',
-                ]
-            },
-            {
-                title: 'ES6', // 必要的
-                collapsable: true, // 可选的, 默认值是 true,
-                sidebarDepth: 2, // 可选的, 默认值是 1
-                children: [
-                    '/blog/es6/1',
-                    '/blog/es6/2',
-                    '/blog/es6/3',
-                    '/blog/es6/4',
-                ]
-            },
-        ]
+        sidebar: {
+            '/blog/': [{
+                    title: 'JavaScript基础', // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: [
+                        '/blog/js-basics/1',
+                        '/blog/js-basics/2',
+                        '/blog/js-basics/3',
+                        '/blog/js-basics/4',
+                        '/blog/js-basics/5',
+                        '/blog/js-basics/6',
+                        '/blog/js-basics/7',
+                        '/blog/js-basics/8',
+                        '/blog/js-basics/9',
+                        '/blog/js-basics/10',
+                    ]
+                },
+                {
+                    title: 'JavaScript专题', // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: [
+                        '/blog/js-special/1',
+                        '/blog/js-special/2',
+                        '/blog/js-special/3',
+                        '/blog/js-special/4',
+                        '/blog/js-special/5',
+                        '/blog/js-special/6',
+                        '/blog/js-special/7',
+                        '/blog/js-special/8',
+                        '/blog/js-special/9',
+                    ]
+                },
+                {
+                    title: 'JavaScript设计模式（待优化）', // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: [
+                        '/blog/js-sjms/1',
+                        '/blog/js-sjms/2',
+                        '/blog/js-sjms/3',
+                        '/blog/js-sjms/4',
+                        '/blog/js-sjms/5',
+                        '/blog/js-sjms/6',
+                    ]
+                },
+                {
+                    title: 'ES6', // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: [
+                        '/blog/es6/1',
+                        '/blog/es6/2',
+                        '/blog/es6/3',
+                        '/blog/es6/4',
+                    ]
+                },
+            ],
+            '/algorithm/': [
+                {
+                    title: '数组', // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: [
+                        '/algorithm/array/1',
+                    ]
+                },
+                {
+                    title: '数组', // 必要的
+                    collapsable: true, // 可选的, 默认值是 true,
+                    sidebarDepth: 2, // 可选的, 默认值是 1
+                    children: [
+                        '/algorithm/array/1',
+                    ]
+                },
+            ]
+        }
+
     }
 }
--- docs/README.md
+++ docs/README.md
@@ -8,7 +8,5 @@ features:
   details: 这里有着前端基础——JavaScript最全面的基础总结，帮助你打牢前端基础！
 - title: 算法与数据结构
   details: 博主正在规划系统的整理算法思想，并与LeetCode练习题相互结合，敬请期待！
-- title: CSS之美
-  details: 这里回和大家一起去探索CSS的魅力，作为前端开发，我们要想艺术家看齐～
 footer: MIT Licensed | Copyright © 2020-present 余光
 ---
