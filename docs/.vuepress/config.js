000a513765dd27be53091b3e0e254c961eb45366
yugaung <yuguang@gaoding.com>
2021-11-30 19:12:23 +0800

    添加actions

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,7 +21,7 @@ module.exports = {
         },
         ads:{
             style: 1, 
-            image: '/1.jpg',
+            image: '/summary/1.jpg',
             text: '前端经典 \n《手撕代码合集》《剑指OfferJs版》都在这里',
             link: 'https://webbj97.github.io/fe-questions/',
           },
