f288d4034765e52c5e8d6d672b6b9044d2daf6f8
yugaung <yuguang@gaoding.com>
2022-05-12 18:30:01 +0800

    链表排序

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -4,5 +4,12 @@ module.exports = [
     { text: '手撕|代码', link: '/handwork/' },
     { text: '算法|题解', link: '/algorithm/' },
     { text: '计算机|基础', link: '/computer/' },
+    {
+        text: '其他',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      },
     { text: '给项目点个Star', link: 'https://github.com/webbj97/summary' },
 ]
