9a5797bcc8b1f18dd1acd9f05a50e4362655d28f
yugaung <yuguang@gaoding.com>
2022-12-24 09:40:42 +0800

    feat: 提交更新

--- docs/blog/ts/demo.ts
+++ docs/blog/ts/demo.ts
@@ -1,14 +1,1 @@
-interface Todo {
-    title: string;
-    description: string;
-    completed: boolean;
-    createdAt: number;
-}
-
-type TodoPreview = Omit<Todo, "description">;
-
-const todo: TodoPreview = {
-    title: "Clean room",
-    completed: false,
-    createdAt: 1615544252770,
-};
+type T0 = Extract<"a" | "b" | "c", "a" | "f">;
