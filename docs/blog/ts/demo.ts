32cadb76b6c475bd67e1100d9f7d32fa68a2b18e
yugaung <yuguang@gaoding.com>
2023-01-09 19:37:12 +0800

    :sparkles: ts类型体操easy 1\2

--- docs/blog/ts/demo.ts
+++ docs/blog/ts/demo.ts
@@ -1,22 +1,18 @@
 type T0 = Extract<"a" | "b" | "c", "a" | "f">;
 
-type numberMsg = {
-    name: string;
-};
-
-type stringMsg = {
-    age: number;
-};
-
-type nameAndAge = numberMsg & stringMsg;
-
-const aaa: nameAndAge = {
-    name: "string",
-};
-
-enum number_d {
-    aaa = 1
-}
-
 // 不能将类型“{ name: string; }”分配给类型“nameAndAge”。
 // 类型 "{ name: string; }" 中缺少属性 "age"，但类型 "stringMsg" 中需要该属性。
+type Per = {
+    name: string;
+    age: number;
+};
+type keyofPer = keyof Per;
+let aa: keyofPer = "name";
+
+type _Pick<T, K extends keyof T> = {
+    [P in K]: T[P];
+};
+
+type _ReadOnly<T> = {
+    readonly [K in keyof T]: T[K];
+};
